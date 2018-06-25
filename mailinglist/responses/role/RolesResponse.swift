5eb68542122ad124bfe629519792d7d5a0720a62
stant <stant@stants-macbook-pro.local>
2018-06-25 19:39:50 -0300

    created response base.

--- mailinglist/responses/role/RolesResponse.swift
+++ mailinglist/responses/role/RolesResponse.swift
@@ -2,15 +2,31 @@ import ObjectMapper
 import Foundation
 
 class DataRoles: Mappable {
-
-     var content: [Role]?
-
+    
+    var content: [Role]?
+    
+    var first: Bool?
+    var last: Bool?
+    var number: Int?
+    var numberOfElements: Int?
+    var totalElements: Int?
+    var totalPages: Int?
+    var sort: Sort?
+    
     required init?(map: Map){
 
     }
 
     func mapping(map: Map) {
         content <- map["content"]
+        first <-   map["first"]
+        last <-   map["last"]
+        number <-   map["number"]
+        numberOfElements <-   map["numberOfElements"]
+        totalElements <-   map["totalElements"]
+        totalPages    <-   map["totalPages"]
+        sort <-   map["sort"]
+
     }
 }
 
