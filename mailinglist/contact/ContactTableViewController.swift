f9752fbfaf2e86e99ee925af0307c92dc9fdaf0c
stant <stant@stants-macbook-pro.local>
2018-07-02 23:11:36 -0300

    delete row implemented

--- mailinglist/contact/ContactTableViewController.swift
+++ mailinglist/contact/ContactTableViewController.swift
@@ -93,18 +93,14 @@ class ContactTableViewController: UITableViewController, ContactViewContract {
         return true
     }
     */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            contacts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+ 
 
     /*
     // Override to support rearranging the table view.
